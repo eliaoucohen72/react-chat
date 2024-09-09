3ca296e71c8376f68cc18383e90ce34fcd73b981
eliyahoc <eliyahoc@shippiz.com>
2024-09-09 14:05:56 +0300

    update gitignore, add the production address to env, add .env file

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const SOCKET_SERVER_URL =
   process.env.NODE_ENV === "production"
-    ? "https://elc-react-chat.up.railway.app"
+    ? import.meta.env.PRODUCTION_SERVER_URL
     : "http://localhost:8080";
 
 export const SOCKET_SERVER_EVENT = "SERVER_MSG";
